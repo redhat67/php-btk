396ca9c763b48d83fe643bf9e8b392c171e66cc3
redhat67 <127238030+redhat67@users.noreply.github.com>
2023-09-19 17:26:02 +0300

    Add files via upload

---
+++ deneme-dosyadizinişlemi5-php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+
+/*   YENİ BİR DOSYA OLUŞTURMA METHODUDUR TOUCH()*/
+$Dosya = "Oluşturulcakdosya.txt";  
+$Olustur = touch($Dosya);
+ 
+
+
+
+
+
+
+
+?>
+</body>
+</html>
