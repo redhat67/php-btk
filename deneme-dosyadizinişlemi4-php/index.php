2c357e7cd6bfc806f410e216feb23a2d0dad6c39
redhat67 <127238030+redhat67@users.noreply.github.com>
2023-09-19 17:25:39 +0300

    Add files via upload

---
+++ deneme-dosyadizinişlemi4-php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+
+
+
+$Dosya = "örnek.zip";
+$İslem = stat($Dosya);
+
+echo "<pre>";
+print_r($İslem);     /* DOSYA HAKKINDA TÜM DEĞER BİLGİLERİNİ BULUP DÖNÜŞTÜRME
+echo "</pre>";
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     ?>
+
+
+
+
+
+
+</body>
+</html>
